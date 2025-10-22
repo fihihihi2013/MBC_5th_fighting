2c6e3d7cd53a4d2ac563468bac84a5b2cbf99342
moon2door <147222951+moon2door@users.noreply.github.com>
2025-10-22 15:59:53 +0900

    Update_kss 2

--- Assets/HelloWorld2.cs
+++ Assets/HelloWorld2.cs
@@ -7,7 +7,7 @@ public class HelloWorld2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("멘토가 수정한 내용입니다");
     }
 
     // Update is called once per frame
