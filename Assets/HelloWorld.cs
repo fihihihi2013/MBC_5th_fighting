cf9e844d361de847c87a5907f5d8a4737dbd18e5
fihihihi2013 <125849576+fihihihi2013@users.noreply.github.com>
2025-10-22 16:04:32 +0900

    멘토님이올린 내용을 풀 해서 커밋푸쉬함

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -8,6 +8,9 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         Debug.Log("¾È³ç°©½À´Ï´Ù");
+        Debug.Log("dd");
+        Debug.Log("°¡³ª´Ù¶ó¸¶¹Ù»ç");
+        Debug.Log("¾È³çÇª¹Ù¿À");
     }
 
     // Update is called once per frame
